2c8d1467929d1f799502dd842f74f9c025cae0b1
emre1n <emre1n@protonmail.com>
2025-07-31 16:01:00 +0300

    add not found component as catch all routes for invalid urls

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { NoTaskComponent } from './tasks/no-task/no-task.component';
 import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { NewTaskComponent } from './tasks/new-task/new-task.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
   {
@@ -21,6 +22,11 @@ export const routes: Routes = [
       {
         path: 'tasks/new', // <your-domain>/users/<user-id>/tasks/new
         component: NewTaskComponent,
+      },
+
+      {
+        path: '**',
+        component: NotFoundComponent,
       },
     ],
   },
