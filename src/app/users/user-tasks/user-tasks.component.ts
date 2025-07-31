c36a16c41489d7bc8d9935627e807b56d166b6f2
emre1n <emre1n@protonmail.com>
2025-07-31 23:22:09 +0300

    resolving route-related dynamic data through resolver

--- src/app/users/user-tasks/user-tasks.component.ts
+++ src/app/users/user-tasks/user-tasks.component.ts
@@ -1,12 +1,11 @@
+import { Component, inject, input } from '@angular/core';
 import {
-  Component,
-  computed,
-  DestroyRef,
-  inject,
-  input,
-  OnInit,
-} from '@angular/core';
-import { ActivatedRoute, RouterLink, RouterOutlet } from '@angular/router';
+  ActivatedRouteSnapshot,
+  ResolveFn,
+  RouterLink,
+  RouterOutlet,
+  RouterStateSnapshot,
+} from '@angular/router';
 
 import { UsersService } from '../users.service';
 
@@ -17,31 +16,19 @@ import { UsersService } from '../users.service';
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
 })
-export class UserTasksComponent implements OnInit {
-  // userId = input.required<string>();
-  userName = '';
+export class UserTasksComponent {
+  userName = input.required<string>();
   message = input.required<string>();
-  private usersService = inject(UsersService);
-  private activatedRoute = inject(ActivatedRoute);
-  private destroyRef = inject(DestroyRef);
-
-  // userName = computed(
-  //   () => this.usersService.users.find((u) => u.id === this.userId())?.name
-  // );
-
-  ngOnInit(): void {
-    console.log('Input Data: ', this.message());
-    console.log(this.activatedRoute);
-
-    const subscription = this.activatedRoute.paramMap.subscribe({
-      next: (paramMap) =>
-        (this.userName =
-          this.usersService.users.find((u) => u.id === paramMap.get('userId'))
-            ?.name || ''),
-    });
-
-    this.destroyRef.onDestroy(() => {
-      subscription.unsubscribe();
-    });
-  }
 }
+
+export const resolveUserName: ResolveFn<string> = (
+  activatedRoute: ActivatedRouteSnapshot,
+  routerState: RouterStateSnapshot
+) => {
+  const usersService = inject(UsersService);
+  const userName =
+    usersService.users.find(
+      (u) => u.id === activatedRoute.paramMap.get('userId')
+    )?.name || '';
+  return userName;
+};
